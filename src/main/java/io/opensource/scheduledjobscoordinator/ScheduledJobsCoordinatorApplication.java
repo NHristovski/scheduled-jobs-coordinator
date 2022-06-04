7b0730be41fda91750d71d0f107e74ef462b5e91
Nikola <nikolahristovski1122@gmail.com>
2022-06-04 22:44:53 +0200

    Initial commit

---
+++ src/main/java/io/opensource/scheduledjobscoordinator/ScheduledJobsCoordinatorApplication.java
@@ -0,0 +1,13 @@
+package io.opensource.scheduledjobscoordinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduledJobsCoordinatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduledJobsCoordinatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/opensource/scheduledjobscoordinator/ScheduledJobsCoordinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.opensource.scheduledjobscoordinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduledJobsCoordinatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
